471d2ed3b708b0516f5f3d2f35cc4d23e1cad924
RUDHRESH S <118143270+iamrudhresh@users.noreply.github.com>
2023-08-05 10:54:43 +0530

    Time: 37 ms (77.45%) | Memory: 32.4 MB (50.20%) - LeetSync

--- 617-merge-two-binary-trees/merge-two-binary-trees.cpp
+++ 617-merge-two-binary-trees/merge-two-binary-trees.cpp
@@ -12,8 +12,11 @@
 class Solution {
 public:
     TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
-        if(root1==nullptr || root2==nullptr){
-            return root1?root1:root2;
+        if(root1==nullptr){
+            return root2;
+        }
+        if(root2==nullptr){
+            return root1;
         }
         root1->val = root1->val + root2->val;
         root1->left = mergeTrees(root1->left,root2->left);
