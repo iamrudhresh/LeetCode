b1802cc5f9e0216eabce3950ec3252246f13a30a
RUDHRESH S <118143270+iamrudhresh@users.noreply.github.com>
2023-09-03 14:54:33 +0530

    Time: 0 ms (100.00%) | Memory: 6 MB (88.67%) - LeetSync

---
+++ 62-unique-paths/unique-paths.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        long long ans=1;
+        for(int i=1;i<=m-1;++i){
+            ans=ans*(n-1+i)/i;
+        }
+        return (int)ans;
+    }
+};
