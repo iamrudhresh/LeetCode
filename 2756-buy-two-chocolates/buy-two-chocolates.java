9dcb866464576f8ec7a705d7cca647c00182e898
RUDHRESH S <118143270+iamrudhresh@users.noreply.github.com>
2023-12-28 20:36:40 +0530

    Time: 5 ms (55.67%) | Memory: 44.4 MB (36.40%) - LeetSync

--- 2756-buy-two-chocolates/buy-two-chocolates.java
+++ 2756-buy-two-chocolates/buy-two-chocolates.java
@@ -4,9 +4,8 @@ class Solution {
         int n=prices.length;
         for(int i=0;i<n-1;i++){
             for(int j=i+1;j<n;j++){
-                if(prices[i]+prices[j]<=money){
-                    int ans=prices[i]+prices[j];
-                    return money-ans;
+                if((prices[i]+prices[j])<=money){
+                    return money-(prices[i]+prices[j]);
                 }
             }
         }
