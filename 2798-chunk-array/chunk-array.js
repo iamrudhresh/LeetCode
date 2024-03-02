36660e007f5984cbf2e49ed2410c31e0e31ce1c3
RUDHRESH S <118143270+iamrudhresh@users.noreply.github.com>
2024-03-02 19:51:22 +0530

    Time: 51 ms (80.80%) | Memory: 51.3 MB (43.72%) - LeetSync

---
+++ 2798-chunk-array/chunk-array.js
@@ -0,0 +1,16 @@
+/**
+ * @param {Array} arr
+ * @param {number} size
+ * @return {Array[]}
+ */
+var chunk = function(arr, size) {
+  var chunkedArray = [];
+  var index = 0;
+
+  while (index < arr.length) {
+    chunkedArray.push(arr.slice(index, index + size));
+    index += size;
+  }
+
+  return chunkedArray;
+}
