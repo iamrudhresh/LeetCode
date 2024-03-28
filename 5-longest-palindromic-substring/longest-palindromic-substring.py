d6d266fc9b0df4b0af2c8e6d155a64bc59971a20
RUDHRESH S <118143270+iamrudhresh@users.noreply.github.com>
2024-03-28 12:05:34 +0530

    Time: 51 ms (99.03%) | Memory: 16.6 MB (84.80%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,18 +1,28 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        if len(s) <= 1:
+        if s[::-1] == s:
             return s
         
-        Max_Len=1
-        Max_Str=s[0]
-        dp = [[False for _ in range(len(s))
-        ] for _ in range(len(s))]
-        for i in range(len(s)):
-            dp[i][i] = True
-            for j in range(i):
-                if s[j] == s[i] and (i-j <= 2 or dp[j+1][i-1]):
-                    dp[j][i] = True
-                    if i-j+1 > Max_Len:
-                        Max_Len = i-j+1
-                        Max_Str = s[j:i+1]
-        return Max_Str
+        # inti start at 0 and window size is 1
+        start, size = 0, 1
+
+        for i in range(1, len(s)):
+            # set up l, r pointer , base on cur idx to left
+            # cur window size = max window size + 1
+            l, r = i - size, i + 1
+
+            # cur window size = max window size + 2
+            s1 = s[l-1:r]
+
+            # if s1 is palindrome, update start idx and the size
+            if l >= 1 and s1[::-1] == s1:
+                size += 2
+                start = l - 1
+            else:
+                # cur window size = max window size + 1
+                s2 = s[l:r]
+                if s2[::-1] == s2:
+                    size += 1
+                    start = l
+        
+        return s[start:start+size]
