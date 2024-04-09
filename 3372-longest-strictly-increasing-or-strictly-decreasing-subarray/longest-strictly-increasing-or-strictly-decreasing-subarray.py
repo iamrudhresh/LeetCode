5d961766a79c2b9700528ca9f7cad77201e16fe2
RUDHRESH S <118143270+iamrudhresh@users.noreply.github.com>
2024-04-09 13:49:26 +0530

    Time: 63 ms (8.07%) | Memory: 16.5 MB (58.55%) - LeetSync

--- 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.py
+++ 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.py
@@ -1,23 +1,30 @@
 class Solution:
     def longestMonotonicSubarray(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
-        
-        increasing_length = 1
-        decreasing_length = 1
-        max_length = 1
-        
-        for i in range(1, len(nums)):
-            if nums[i] > nums[i - 1]:
-                increasing_length += 1
-                decreasing_length = 1
-            elif nums[i] < nums[i - 1]:
-                decreasing_length += 1
-                increasing_length = 1
-            else:
-                increasing_length = 1
-                decreasing_length = 1
+        longinc=1
+        longdec=1
+        i=0
+        j=0
+        for i in range(len(nums)-1):
+            if(i<len(nums)-1 and nums[i]<nums[i+1]):
+                j=i
+                while(j<len(nums)-1 and nums[j]<nums[j+1]):
+                    j+=1
+                longdec=max(j-i+1,longdec)
+                i=j
+            i+=1
+        i=0
+        j=0
+        for i in range(len(nums)-1):
+            if(i<len(nums)-1 and nums[i]>nums[i+1]):
+                j=i
+                while(j<len(nums)-1 and nums[j]>nums[j+1]):
+                    j+=1
+                longinc=max(j-i+1,longinc)
+                i=j
+            i+=1
+        return max(longdec,longinc)
             
-            max_length = max(max_length, increasing_length, decreasing_length)
+            
+                    
+                
         
-        return max_length
