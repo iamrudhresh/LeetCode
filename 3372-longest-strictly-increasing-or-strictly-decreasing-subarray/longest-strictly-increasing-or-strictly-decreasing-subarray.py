cf0a8d908d03799dabd20121d720287a7529409e
RUDHRESH S <118143270+iamrudhresh@users.noreply.github.com>
2024-04-09 13:50:36 +0530

    Time: 52 ms (32.69%) | Memory: 16.5 MB (58.55%) - LeetSync

--- 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.py
+++ 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.py
@@ -1,30 +1,20 @@
 class Solution:
     def longestMonotonicSubarray(self, nums: List[int]) -> int:
-        longinc=1
-        longdec=1
-        i=0
-        j=0
-        for i in range(len(nums)-1):
-            if(i<len(nums)-1 and nums[i]<nums[i+1]):
-                j=i
-                while(j<len(nums)-1 and nums[j]<nums[j+1]):
-                    j+=1
-                longdec=max(j-i+1,longdec)
-                i=j
-            i+=1
-        i=0
-        j=0
-        for i in range(len(nums)-1):
-            if(i<len(nums)-1 and nums[i]>nums[i+1]):
-                j=i
-                while(j<len(nums)-1 and nums[j]>nums[j+1]):
-                    j+=1
-                longinc=max(j-i+1,longinc)
-                i=j
-            i+=1
-        return max(longdec,longinc)
-            
-            
-                    
-                
+        if not nums:
+            return 0
+        inc = 1
+        dec = 1
+        maxl = 1
+        for i in range(1,len(nums)):
+            if nums[i] > nums[i-1]:
+                inc += 1
+                dec = 1
+            elif nums[i] < nums[i-1]:
+                dec += 1
+                inc = 1
+            else:
+                inc = 1
+                dec = 1
+            maxl = max(maxl,dec,inc)
+        return maxl
         
